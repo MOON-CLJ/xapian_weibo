e02982b3e8da815a6cfec48169f2d914ba010ecd
MOON_CLJ <lijunli2598@gmail.com>
2012-05-26 18:17:41 +0800

    haha

--- simplesearch.py
+++ simplesearch.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+#import sys
 import xapian
 import time
 import datetime
 import simplejson as json
-from collections import Counter
+#from collections import Counter
 import collections
 import itertools
 import multiprocessing
+
 
 class SimpleMapReduce(object):
     def __init__(self, map_func, reduce_func, num_workers=None):
         self.map_func = map_func
         self.reduce_func = reduce_func
-        self.pool = multiprocessing.Pool(num_workers)
+        num_workers = multiprocessing.cpu_count() * 2
+        self.pool = multiprocessing.Pool(num_workers, maxtasksperchild=10)
 
     def partition(self, mapped_values):
         partitioned_data = collections.defaultdict(list)
@@ -29,15 +31,18 @@ class SimpleMapReduce(object):
         reduced_values = self.pool.map(self.reduce_func, partitioned_data)
         return reduced_values
 
-def hasharr_to_list(keywords_hash):
+
+def hasharr_to_list(keywords_arr):
         keywords_list = []
-        for keyword in keywords_hash:
-            keywords_list.append((keyword,keywords_hash[keyword]))
+        for keyword in keywords_arr:
+            keywords_list.append((keyword, keywords_arr[keyword]))
         return keywords_list
+
 
 def count_words(item):
         word, occurances = item
         return (word, sum(occurances))
+
 
 class WeiboSearch(object):
     def __init__(self, dbpath='simple'):
@@ -63,22 +68,21 @@ class WeiboSearch(object):
         #widvi = 10
         self.maxitems = 1000000000
 
-    def query(self,querystring=None,qtype=None,begin=None,end=None,keywords=[],hashtags=[],emotiononly=False):
+    def query(self, querystring=None, qtype=None, begin=None, end=None, keywords=[], hashtags=[], synonymslist=[], emotiononly=False):
         if qtype == 'hy':
             self.qp.add_valuerangeprocessor(xapian.NumberValueRangeProcessor(self.timestampvi, ''))
-            querystring = begin+'..'+end
+            querystring = begin + '..' + end
 
             if emotiononly:
-                self.qp.add_valuerangeprocessor(xapian.NumberValueRangeProcessor(self.emotiononlyvi,'f',False))
+                self.qp.add_valuerangeprocessor(xapian.NumberValueRangeProcessor(self.emotiononlyvi, 'f', False))
                 querystring += ' 1.0..1.0f'
 
             query = self.qp.parse_query(querystring)
             print "Parsed query is: %s" % [str(query)]
 
             self.enquire.set_query(query)
-            matches = self.enquire.get_mset(0,self.maxitems)
+            matches = self.enquire.get_mset(0, self.maxitems)
             #matches = self.enquire.get_mset(0,10)
-
 
             # Display the results.
             print "%i results found." % matches.size()
@@ -94,36 +98,36 @@ class WeiboSearch(object):
                 keywords_arr.append(keywords_hash)
                 #keywords_counter += Counter(json.loads(m.document.get_value(self.keywordsvi)))
 
-            print 'mapreduce begin: ',str(time.strftime("%H:%M:%S", time.gmtime()))
+            print 'mapreduce begin: ', str(time.strftime("%H:%M:%S", time.gmtime()))
             mapper = SimpleMapReduce(hasharr_to_list, count_words)
             #keywords_arr = [{'haha':1,"haha":2},{'haha':3}]
             word_counts = mapper(keywords_arr)
             lowkeywords_set = set()
-            for word,count in word_counts:
+            for word, count in word_counts:
                 if count <= 3:
                     lowkeywords_set.add(word)
-            print 'mapreduce end: ',str(time.strftime("%H:%M:%S", time.gmtime()))
+            print 'mapreduce end: ', str(time.strftime("%H:%M:%S", time.gmtime()))
 
             keywords_list = []
             for x in keywords_arr:
                 per_keywords_list = []
                 for keyword in x:
                     if keyword not in lowkeywords_set:
-                        per_keywords_list.extend([keyword]*x[keyword])
+                        per_keywords_list.extend([keyword] * x[keyword])
                 keywords_list.append(per_keywords_list)
 
             #print keywords_counter
-            return emotions_list,keywords_list
+            return emotions_list, keywords_list
 
         if qtype == 'yq':
             self.qp.add_valuerangeprocessor(xapian.NumberValueRangeProcessor(self.timestampvi, ''))
-            querystring = begin+'..'+end
+            querystring = begin + '..' + end
             query = self.qp.parse_query(querystring)
             print "Parsed query is: %s" % [str(query)]
 
             self.enquire.set_query(query)
             #matches = self.enquire.get_mset(0,10)
-            matches = self.enquire.get_mset(0,self.maxitems)
+            matches = self.enquire.get_mset(0, self.maxitems)
 
             # Display the results.
             print "%i results found." % matches.size()
@@ -138,37 +142,43 @@ class WeiboSearch(object):
                 keywords_arr.append(keywords_hash)
                 #keywords_counter += Counter(json.loads(m.document.get_value(self.keywordsvi)))
 
-            print 'mapreduce begin: ',str(time.strftime("%H:%M:%S", time.gmtime()))
+            print 'mapreduce begin: ', str(time.strftime("%H:%M:%S", time.gmtime()))
             mapper = SimpleMapReduce(hasharr_to_list, count_words)
             word_counts = mapper(keywords_arr)
             keywords_hash = {}
-            for word,count in word_counts:
+            for word, count in word_counts:
                 keywords_hash[word] = count
-            print 'mapreduce end: ',str(time.strftime("%H:%M:%S", time.gmtime()))
+            for synonyms in synonymslist:
+                if len(synonyms) >= 2 and synonyms[0] in keywords_hash:
+                    for word in synonyms[1:]:
+                        if word in keywords_hash:
+                            keywords_hash[synonyms[0]] += keywords_hash[word]
+                            del keywords_hash[word]
+            print 'mapreduce end: ', str(time.strftime("%H:%M:%S", time.gmtime()))
 
             #print keywords_counter
-            return hashtags,keywords_hash
+            return hashtags, keywords_hash
 
         if qtype == 'lh':
             self.qp.add_valuerangeprocessor(xapian.NumberValueRangeProcessor(self.timestampvi, ''))
-            timequerystr = begin+'..'+end
+            timequerystr = begin + '..' + end
             timequery = self.qp.parse_query(timequerystr)
 
             hashtags = ['H' + hashtag.lower() for hashtag in hashtags]
             keywords = [keyword.lower() for keyword in keywords]
             keywords.extend(hashtags)
             if len(keywords) > 0:
-                wordsquery = xapian.Query(xapian.Query.OP_OR,keywords)
+                wordsquery = xapian.Query(xapian.Query.OP_OR, keywords)
             else:
                 return None
 
-            query = xapian.Query(xapian.Query.OP_AND,[timequery,wordsquery])
+            query = xapian.Query(xapian.Query.OP_AND, [timequery, wordsquery])
             print "Parsed query is: %s" % [str(query)]
 
             self.enquire.set_query(query)
-            self.enquire.set_sort_by_value(self.timestampvi,False)
+            self.enquire.set_sort_by_value(self.timestampvi, False)
             #matches = self.enquire.get_mset(0,10)
-            matches = self.enquire.get_mset(0,self.maxitems)
+            matches = self.enquire.get_mset(0, self.maxitems)
 
             # Display the results.
             print "%i results found." % matches.size()
