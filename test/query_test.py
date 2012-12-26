62b379a4ccd7692a970fe067973df223e8b1faa9
MOON_CLJ <lijunli2598@gmail.com>
2012-12-26 09:58:36 +0800

    增加测试query

--- test/query_test.py
+++ test/query_test.py
@@ -1,12 +1,15 @@
 # -*- coding:utf-8 -*-
 
 import sys
+import  calendar
+import datetime
 
 sys.path.append('../xapian_weibo')
 from xapian_backend import XapianSearch
 
 s = XapianSearch(path='../data/', name='statuses')
 
+"""
 results = s.search(query={'text': [u'中国'], 'uid': 1217743083, 'ts': {'$gt': 0, '$lt': 1334450340}}, sort_by=['-ts'], fields=['text', 'ts', 'name'])
 
 print 'query1:'
@@ -19,7 +22,7 @@ print 'hits: %s' % results['hits']
 print 'query2:'
 query_dict = {'$and': {'text': [u'中国'],
                        'uid': 1217743083,
-                       '$or': {'ts': {'gt': 0, 
+                       '$or': {'ts': {'gt': 0,
                                       'lt': 1334450340},
                                'uid': 0000000000}},
               '$not': {'text': u'宝马', 'name': u'白之兔'},
@@ -32,6 +35,24 @@ for r in results['results']:
     print r['text']
 
 print 'hits: %s' % results['hits']
+"""
 
+begin_ts1 = calendar.timegm(datetime.datetime(2011, 10, 1).timetuple())
+end_ts1 = calendar.timegm(datetime.datetime(2011, 12, 31).timetuple())
+begin_ts2 = calendar.timegm(datetime.datetime(2010, 10, 1).timetuple())
+end_ts2 = calendar.timegm(datetime.datetime(2010, 12, 31).timetuple())
 
-
+query_dict = {
+    '$or': {
+        'ts': {
+            '$gt': begin_ts1,
+            '$lt': end_ts1,
+        },
+        'ts': {
+            '$gt': begin_ts2,
+            '$lt': end_ts2,
+        },
+    }
+}
+results = s.search(query=query_dict)
+print results['hits']
