b3d60a5cd9c6ccbcffeb0c5d7b29380ed9f7d00a
MOON_CLJ <lijunli2598@gmail.com>
2013-01-01 16:42:32 +0800

    add test

--- test/query_test.py
+++ test/query_test.py
@@ -9,6 +9,7 @@ from xapian_backend import XapianSearch
 
 s = XapianSearch(path='../data/', name='statuses')
 
+"""
 results = s.search(query={'text': [u'中国'], 'uid': 1217743083, 'ts': {'$gt': 0, '$lt': 1334450340}}, sort_by=['-ts'], fields=['text', 'ts', 'name'])
 
 print 'query1:'
@@ -18,38 +19,44 @@ for r in results['results']:
 
 print 'hits: %s' % results['hits']
 
+"""
+
+"""
 print 'query2:'
 query_dict = {'$and': [{'text': [u'中国'], 'uid': 1217743083},
                        {'uid': 1217743083},
                        {'$or': [{'ts': {'gt': 0,
-                                      'lt': 1334450340}},
+                                        'lt': 1334450340}},
                                 {'uid': 0000000000}]}],
               '$not': {'text': u'宝马', 'name': u'白之兔'},
               'name': u'袁岳'
               }
 
-results = s.search(query=query_dict, sort_by=['-ts'], fields=['text', 'ts'])
+results = s.search(query=query_dict, sort_by=['-ts'], fields=['text', 'ts', 'name'])
 
 for r in results['results']:
     print r['text']
+    print r['name']
 
 print 'hits: %s' % results['hits']
 print s.parse_query(query_dict)
-# begin_ts1 = calendar.timegm(datetime.datetime(2011, 10, 1).timetuple())
-# end_ts1 = calendar.timegm(datetime.datetime(2011, 12, 31).timetuple())
-# begin_ts2 = calendar.timegm(datetime.datetime(2010, 10, 1).timetuple())
-# end_ts2 = calendar.timegm(datetime.datetime(2010, 12, 31).timetuple())
+"""
 
-# query_dict = {'$or': 
-#               [{'ts': {
-#                 '$gt': begin_ts1,
-#                 '$lt': end_ts1,
-#                 }},
-#                {'ts': {
-#                 '$gt': begin_ts2,
-#                 '$lt': end_ts2,
-#                 }}]}
-# results = s.search(query=query_dict, fields=['uid', 'text', 'ts', 'name', 'emotions'])
-# for r in results['results']:
-#     print r['emotions']
-# print results['hits']
+begin_ts1 = calendar.timegm(datetime.datetime(2011, 10, 1).timetuple())
+end_ts1 = calendar.timegm(datetime.datetime(2011, 12, 31).timetuple())
+begin_ts2 = calendar.timegm(datetime.datetime(2010, 10, 1).timetuple())
+end_ts2 = calendar.timegm(datetime.datetime(2010, 12, 31).timetuple())
+
+query_dict = {'$or':
+              [{'ts': {
+                '$gt': begin_ts1,
+                '$lt': end_ts1,
+                }},
+               {'ts': {
+                '$gt': begin_ts2,
+                '$lt': end_ts2,
+                }}]}
+results = s.search(query=query_dict, fields=['uid', 'text', 'ts', 'name', 'emotions'])
+for r in results['results']:
+    print r['emotions']
+print results['hits']
