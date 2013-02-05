46a842d59716bc6b54083fd3717548dda2871091
MOON_CLJ <lijunli2598@gmail.com>
2013-02-05 14:16:56 +0800

    setup.py more useful

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@ from setuptools import setup
 
 setup(name='xapian_weibo',
       version='0.1',
-      author='MOON_CLJ',
+      url='https://github.com/MOON-CLJ/xapian_weibo',
+      author='CLJ',
       packages=['xapian_weibo'],
-      )
+      install_requires=[
+          'pymongo',
+      ],
+      dependency_links=[
+      ],
+)
+
+# extra requires
+# xapian and python-binding
+# git@github.com:MOON-CLJ/pyscws.git
