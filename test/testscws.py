01bcac17c69658ba519f8a54a7bc4a1fe7bef473
MOON_CLJ <lijunli2598@gmail.com>
2013-02-06 18:17:10 +0800

    little change

--- test/testscws.py
+++ test/testscws.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
-import time
-import scws
+from xapian_weibo.utils import load_scws
 
 text = '加加加加加加加加'  # * 55000
 
-s = scws.Scws()
-s.set_charset('utf-8')
-s.set_dict('/usr/local/scws/etc/dict.utf8.xdb', scws.XDICT_MEM)
-s.add_dict('/usr/local/scws/etc/dict_cht.utf8.xdb', scws.XDICT_MEM)
-s.add_dict('../dict/userdic.txt', scws.XDICT_TXT)
-s.add_dict('../dict/stopword.dic', scws.XDICT_TXT)
-s.add_dict('../dict/emotionlist.txt', scws.XDICT_TXT)
-
-s.set_rules('/usr/local/scws/etc/rules.utf8.ini')
-s.set_ignore(1)
+text = '我们是好朋友'
+s = load_scws()
 
 for word in s.participle(text):
     print word[0]
