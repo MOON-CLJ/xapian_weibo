224698d8638c3d8caa453f82d94eb3a6e3b15fa0
MOON_CLJ <lijunli2598@gmail.com>
2013-05-09 23:58:47 +0800

    add plyvel_multi_write_weibos

--- test/test_plyvel.py
+++ test/test_plyvel.py
@@ -3,6 +3,8 @@
 
 import time
 import plyvel
+import simplejson as json
+from utils4scrapy.tk_maintain import _default_mongo
 
 
 def timeit(method):
@@ -10,7 +12,7 @@ def timeit(method):
         ts = time.time()
         result = method(*args, **kw)
         te = time.time()
-        print '%r args: %s %2.2f sec' % (method.__name__, args, te - ts)
+        print '%r %2.2f sec' % (method.__name__, te - ts)
         return result
     return timed
 
@@ -34,6 +36,24 @@ def test_plyvel_multi_write(n):
             wb.put(str(i), str(i))
 
 
+@timeit
+def load_weibos_from_mongo(limit):
+    weibos = []
+    mongo = _default_mongo(usedb='master_timeline')
+    for weibo in mongo.master_timeline_weibo.find().limit(limit):
+        weibos.append(weibo)
+
+    print 'load', len(weibos), 'weibos'
+    return weibos
+
+
+@timeit
+def plyvel_multi_write_weibos(weibos):
+    with db.write_batch() as wb:
+        for weibo in weibos:
+            wb.put(str(weibo['id']), json.dumps(weibo))
+
+
 if __name__ == '__main__':
     n = 1000000
 
@@ -51,6 +71,11 @@ if __name__ == '__main__':
     test_plyvel_single_write(n)
     test_plyvel_single_read(n)
     test_plyvel_multi_write(n)
+
+    weibos_from_mongo = load_weibos_from_mongo(n)
+    db = plyvel.DB('./plyvel3', create_if_missing=True, lru_cache_size=8 * (2 << 25), write_buffer_size=8 * (2 << 25), bloom_filter_bits=10)
+    plyvel_multi_write_weibos(weibos_from_mongo)
+
     """
     'test_plyvel_single_write' args: (1000000,) 17.66 sec
     'test_plyvel_single_read' args: (1000000,) 3.92 sec
@@ -64,4 +89,8 @@ if __name__ == '__main__':
     说明跟pyleveldb一样，调整参数对单条写影响很大
     plyvel性能优于pyleveldb
     plyvel似乎真正起到作用，不像pyleveldb
+
+    load 1000000 weibos
+    'load_weibos_from_mongo' 41.69 sec
+    'plyvel_multi_write_weibos' 57.56 sec
     """
