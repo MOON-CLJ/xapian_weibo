53aefa4bf4ca7999eeffd3a24363c086e9ca3716
MOON_CLJ <lijunli2598@gmail.com>
2013-05-10 15:07:52 +0800

    确定simplejson与msgpack对比测试结果,确定往msgpack上转

--- test/bench_simplejson_msgpack.py
+++ test/bench_simplejson_msgpack.py
@@ -8,7 +8,7 @@ from utils4scrapy.tk_maintain import _default_mongo
 
 class BenchSimplejsonMsgpack(hurdles.BenchCase):
     def setUp(self):
-        n = 1000
+        n = 100000
         self.weibos_to_encode = self._load_weibos_from_mongo(n)
         self.weibos_to_decode_json = [json.dumps(weibo) for weibo in self.weibos_to_encode]
         self.weibos_to_decode_msgpack = [msgpack.packb(weibo) for weibo in self.weibos_to_encode]
@@ -40,3 +40,35 @@ class BenchSimplejsonMsgpack(hurdles.BenchCase):
     def bench_msgpack_unpackb(self, *args, **kwargs):
         for weibo in self.weibos_to_decode_msgpack:
             msgpack.unpackb(weibo)
+
+    """
+    hurdles bench_simplejson_msgpack.py
+    load 100000 weibos
+    BenchSimplejsonMsgpack.bench_msgpack_packb
+    | average       4804.949 ms
+    | median        4805.27 ms
+    | fastest       4800.99 ms
+    | slowest       4807.43 ms
+    load 100000 weibos
+    BenchSimplejsonMsgpack.bench_msgpack_unpackb
+    | average       1271.494 ms
+    | median        1271.38 ms
+    | fastest       1269.81 ms
+    | slowest       1274.86 ms
+    load 100000 weibos
+    BenchSimplejsonMsgpack.bench_simplejson_dumps
+    | average       6369.791 ms
+    | median        6365.03 ms
+    | fastest       6353.45 ms
+    | slowest       6427.96 ms
+    load 100000 weibos
+    BenchSimplejsonMsgpack.bench_simplejson_loads
+    | average       6969.229 ms
+    | median        6943.07 ms
+    | fastest       6913.58 ms
+    | slowest       7087.94 ms
+
+    ------------------------------------------------------------
+    Ran 4 benchmarks
+    说明msgpack比simplejson要快，确定往上面转
+    """
