c7e5c597f4ff6916b6fc59135480eaef4ca1e17c
MOON_CLJ <lijunli2598@gmail.com>
2013-05-10 21:53:26 +0800

    加入user的schema

--- xapian_weibo/xapian_backend.py
+++ xapian_weibo/xapian_backend.py
@@ -494,7 +494,7 @@ class Schema:
     v2 = {
         'db': 'master_timeline',
         'collection': 'master_timeline_weibo',
-        'dumps_exclude': ['id', 'mid', 'created_at', 'source', 'comments_count', 'attitudes_count', 'bmiddle_pic', 'original_pic', 'hashtags', 'emotions', 'urls', 'at_users', 'repost_users', 'reposts', 'comments', 'first_in', 'last_modify'],
+        'dumps_exclude': ['id', 'mid', 'created_at', 'original_pic', 'hashtags', 'emotions', 'urls', 'at_users', 'repost_users', 'reposts', 'comments', 'first_in', 'last_modify'],
         'pre': {
             'retweeted_status': lambda x: x['id'],
             'user': lambda x: x['id']
@@ -512,6 +512,24 @@ class Schema:
             {'field_name': '_id', 'column': 3, 'type': 'long'},
             {'field_name': 'timestamp', 'column': 4, 'type': 'long'},
             {'field_name': 'reposts_count', 'column': 5, 'type': 'long'},
+        ],
+    }
+
+    v3 = {
+        'db': 'master_timeline',
+        'collection': 'master_timeline_user',
+        'dumps_exclude': ['id', 'first_in', 'last_modify'],
+        'obj_id': '_id',
+        # 用于去重的value no(column)
+        'collapse_valueno': 3,
+        'idx_fields': [
+            # term
+            {'field_name': 'name', 'column': 0, 'type': 'text'},
+            # value
+            {'field_name': 'followers_count', 'column': 1, 'type': 'long'},
+            {'field_name': 'statuses_count', 'column': 2, 'type': 'long'},
+            {'field_name': 'friends_count', 'column': 3, 'type': 'long'},
+            {'field_name': 'bi_followers_count', 'column': 4, 'type': 'long'},
         ],
     }
 
