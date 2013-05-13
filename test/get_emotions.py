0b985643a737d050b28d5f62468e8ad25c48f818
MOON_CLJ <lijunli2598@gmail.com>
2013-05-13 22:09:34 +0800

    更新emotions

--- test/get_emotions.py
+++ test/get_emotions.py
@@ -1,7 +1,7 @@
 import urllib
 import simplejson as json
 
-res = urllib.urlopen('https://api.weibo.com/2/emotions.json?access_token=2.00OGiDA48EJCd272c68c88FKpILC')
+res = urllib.urlopen('https://api.weibo.com/2/emotions.json?access_token=2.00z5vGCDyoqaVE13ca298d600LL9mR')
 data = res.read()
 data = json.loads(data)
 
