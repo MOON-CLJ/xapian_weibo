dc9c885ae305f6b32a69eb6ba48b7cf45b0c9e1f
MOON_CLJ <lijunli2598@gmail.com>
2013-05-16 16:24:57 +0800

    add 转发user建索引代码

--- xapian_weibo/xapian_backend_extra.py
+++ xapian_weibo/xapian_backend_extra.py
@@ -3,9 +3,10 @@
 
 from argparse import ArgumentParser
 from xapian_backend import timeit, _marshal_value, _marshal_term, _database, InvalidIndexError
-from xapian_backend import XapianSearch as XapianSearchWeibo
+from xapian_backend import XapianSearch
 import os
 import sys
+import re
 import xapian
 import leveldb
 import msgpack
@@ -30,8 +31,11 @@ class XapianIndex(object):
 
         self.databases = {}
         self.ts_and_dbfolders = []
-        self.weibo_multi_sentiment_bucket = leveldb.LevelDB(os.path.join(LEVELDBPATH, 'huyue_weibo_multi_sentiment'),
-                                                            block_cache_size=8 * (2 << 25), write_buffer_size=8 * (2 << 25))
+        if schema_version == 1:
+            self.weibo_multi_sentiment_bucket = leveldb.LevelDB(os.path.join(LEVELDBPATH, 'huyue_weibo_multi_sentiment'),
+                                                                block_cache_size=8 * (2 << 25), write_buffer_size=8 * (2 << 25))
+        elif schema_version == 2:
+            self.xapian_search_user = XapianSearch(path='/opt/xapian_weibo/data/', name='master_timeline_user')
 
     def document_count(self, folder):
         try:
@@ -61,7 +65,12 @@ class XapianIndex(object):
     def index_items(self):
         count = 0
         try:
-            for item in _load_weibos_from_xapian(fields=['_id', 'user', 'terms', 'timestamp']):
+            # 如果是情绪的只load最近90天的，否则load全部
+            if SCHEMA_VERSION == 1:
+                get_results = _load_weibos_from_xapian(fields=['_id', 'user', 'terms', 'timestamp'])
+            elif SCHEMA_VERSION == 2:
+                get_results = _load_weibos_from_xapian(total_days=3650, fields=['_id', 'user', 'text', 'terms', 'timestamp'])
+            for item in get_results():
                 count += 1
                 if 'posted_at_key' not in self.schema:
                     raise Exception('当前mode下需要schema里包含区分folder的posted_at_key')
@@ -93,35 +102,54 @@ class XapianIndex(object):
         for field in self.schema['idx_fields']:
             self.index_field(field, document, item, SCHEMA_VERSION)
 
-        document.set_data(msgpack.packb({}))
+        self.set_data(document, item, SCHEMA_VERSION)
         document.add_term(document_id)
         self.get_database(folder).replace_document(document_id, document)
         #self.get_database(folder).add_document(document)
 
+    def set_data(self, document, item, schema_version):
+        if schema_version == 1:
+            document.set_data(msgpack.packb({}))
+        elif schema_version == 2:
+            re_result = re.search(r'//@(\S+?):', item['text'])
+            name = re_result.group(1) if re_result else None
+            reposted_user = 0
+            if name:
+                _, get_results = self.xapian_search_user.search(query={'name': name}, max_offset=1, fields=['_id'])
+                get_results = list(get_results())
+                if get_results:
+                    reposted_user = get_results[0]['_id']
+            document.set_data(msgpack.packb({'reposted_user': reposted_user}))
+
     def index_field(self, field, document, item, schema_version):
-        _index_field(field, document, item, schema_version, self.schema, self.weibo_multi_sentiment_bucket)
+        if schema_version == 1:
+            _index_field(field, document, item, schema_version, self.schema, self.weibo_multi_sentiment_bucket)
+        elif schema_version == 2:
+            _index_field(field, document, item, schema_version, self.schema)
 
 
-def _index_field(field, document, item, schema_version, schema, weibo_multi_sentiment_bucket):
+def _index_field(field, document, item, schema_version, schema, weibo_multi_sentiment_bucket=None):
     prefix = DOCUMENT_CUSTOM_TERM_PREFIX + field['field_name'].upper()
-    if schema_version == 1:
-        # 必选term
-        if field['field_name'] in ['user']:
-            term = _marshal_term(item[field['field_name']])
-            document.add_term(prefix + term)
-        elif field['field_name'] == 'sentiment':
-            sentiment = weibo_multi_sentiment_bucket.get(str(item[schema['obj_id']]))
-            sentiment = int(sentiment)
-            term = _marshal_term(sentiment)
-            document.add_term(prefix + term)
-        # value
-        elif field['field_name'] in ['_id', 'timestamp']:
-            document.add_value(field['column'], _marshal_value(item[field['field_name']]))
-        elif field['field_name'] == 'text':
-            tokens = item['terms']
-            termgen = xapian.TermGenerator()
-            termgen.set_document(document)
-            termgen.index_text_without_positions(' '.join(tokens), 1, prefix)
+    # if schema_version == 1:
+    # 两种schema的检索条件兼容
+
+    # 必选term
+    if field['field_name'] in ['user']:
+        term = _marshal_term(item[field['field_name']])
+        document.add_term(prefix + term)
+    elif field['field_name'] == 'sentiment':
+        sentiment = weibo_multi_sentiment_bucket.get(str(item[schema['obj_id']]))
+        sentiment = int(sentiment)
+        term = _marshal_term(sentiment)
+        document.add_term(prefix + term)
+    # value
+    elif field['field_name'] in ['_id', 'timestamp']:
+        document.add_value(field['column'], _marshal_value(item[field['field_name']]))
+    elif field['field_name'] == 'text':
+        tokens = item['terms']
+        termgen = xapian.TermGenerator()
+        termgen.set_document(document)
+        termgen.index_text_without_positions(' '.join(tokens), 1, prefix)
 
 
 @timeit
@@ -133,7 +161,7 @@ def _load_weibos_from_xapian(total_days=90, fields=['_id', 'retweeted_status', '
     query_dict = {
         'timestamp': {'$gt': begin_ts, '$lt': end_ts},
     }
-    s = XapianSearchWeibo(path='/opt/xapian_weibo/data/', name='master_timeline_weibo')
+    s = XapianSearch(path='/opt/xapian_weibo/data/', name='master_timeline_weibo')
 
     count, get_results = s.search(query=query_dict, fields=fields)
     print count
@@ -158,6 +186,20 @@ class Schema:
         ],
     }
 
+    v2 = {
+        'obj_id': '_id',
+        # 用于去重的value no(column)
+        'collapse_valueno': 2,
+        'posted_at_key': 'timestamp',
+        'idx_fields': [
+            # term
+            {'field_name': 'user', 'column': 0, 'type': 'long'},
+            {'field_name': 'text', 'column': 1, 'type': 'text'},
+            # value
+            {'field_name': '_id', 'column': 2, 'type': 'long'},
+            {'field_name': 'timestamp', 'column': 3, 'type': 'long'},
+        ],
+    }
 
 if __name__ == '__main__':
     parser = ArgumentParser()
