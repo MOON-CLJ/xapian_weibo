b201ee756eb48b696260e0618fbc7105011a8a3c
MOON_CLJ <lijunli2598@gmail.com>
2013-10-07 19:55:07 +0800

    统一consts到prod模式

--- xapian_weibo/consts.py
+++ xapian_weibo/consts.py
@@ -13,13 +13,16 @@ elif XAPIAN_INDEX_SCHEMA_VERSION == 1:
 elif XAPIAN_INDEX_SCHEMA_VERSION == 3:
     XAPIAN_DB_PATH = 'master_timeline_sentiment'
 
-PROD_VENV = 0
+PROD_VENV = 1
 if PROD_VENV:
     XAPIAN_DATA_DIR = '/var/lib/xapian_weibo'
     XAPIAN_STUB_FILE_DIR = '/var/lib/xapian_weibo/stub'
-    XAPIAN_ZMQ_VENT_HOST = ''
+    XAPIAN_ZMQ_VENT_HOST = '219.224.135.61'
     XAPIAN_FLUSH_DB_SIZE = 20000
-    BSON_FILEPATH = '/home/arthas/mongodumps/20130516/master_timeline/master_timeline_weibo.bson'
+    if XAPIAN_INDEX_SCHEMA_VERSION == 2:
+        BSON_FILEPATH = '/home/arthas/mongodumps/20130516/master_timeline/master_timeline_weibo.bson'
+    elif XAPIAN_INDEX_SCHEMA_VERSION == 1:
+        BSON_FILEPATH = '/home/arthas/mongodumps/20130516/master_timeline/master_timeline_user.bson'
 else:
     XAPIAN_DATA_DIR = '/home/arthas/dev/xapian_weibo/data'
     XAPIAN_STUB_FILE_DIR = '/home/arthas/dev/xapian_weibo/stub'
