2fdefe197daa378d2ebad01b314e2b3fe4988507
MOON_CLJ <lijunli2598@gmail.com>
2013-11-07 11:40:43 +0800

    2 test results add

--- benchs/iter_all_docs/test_stub_file_open_tcp.py
+++ benchs/iter_all_docs/test_stub_file_open_tcp.py
@@ -1,49 +1,13 @@
 # -*- coding:utf-8 -*-
 
 import sys
-import time
 import datetime
 
-sys.path.append('../xapian_weibo')
+sys.path.append('../../xapian_weibo')
 from xapian_backend import XapianSearch
 
-"""
-print "query1:"
-
-stub = '/home/mirage/clj/dev/xapian_weibo/tests/master_timeline_weibo_20131104'
-s = XapianSearch(stub=stub, include_remote=True)
+stub = 'master_timeline_weibo_stub'
+s = XapianSearch(stub=stub)
 results = s.iter_all_docs()
-print 'hehe'
-count = 0
-te = ts = time.time()
 for r in results:
-    count += 1
-    if count % 10000 == 0:
-        te = time.time()
-        print te - ts
-        ts = te
-print 'hits: ', count
-"""
-
-print "query2:"
-
-stub = '/home/mirage/clj/dev/xapian_weibo/tests/master_timeline_weibo_20131104'
-s = XapianSearch(stub=stub, include_remote=True)
-begin_ts1 = time.mktime(datetime.datetime(2013, 1, 1).timetuple())
-end_ts1 = time.mktime(datetime.datetime(2013, 1, 15).timetuple())
-
-query_dict = {
-    'timestamp': {'$gt': begin_ts1, '$lt': end_ts1},
-    '$not': {'retweeted_status': '0'}
-}
-count, get_results = s.search(query=query_dict, fields=['user'])
-print 'hints', count
-
-count = 0
-te = ts = time.time()
-for r in get_results():
-    count += 1
-    if count % 100000 == 0:
-        te = time.time()
-        print te - ts
-        ts = te
+    pass
---
+++ benchs/search/test_stub_file_open_tcp.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import time
+import datetime
+
+sys.path.append('../../xapian_weibo')
+from xapian_backend import XapianSearch
+
+stub = 'master_timeline_weibo_stub'
+s = XapianSearch(stub=stub)
+begin_ts1 = time.mktime(datetime.datetime(2013, 1, 1).timetuple())
+end_ts1 = time.mktime(datetime.datetime(2013, 1, 15).timetuple())
+
+query_dict = {
+    'timestamp': {'$gt': begin_ts1, '$lt': end_ts1},
+    '$not': {'retweeted_status': '0'}
+}
+_, get_results = s.search(query=query_dict, fields=['user'])
+
+for r in get_results():
+    pass
