f6f16af9d8923067c27530859cc57ef8d17344e4
MOON_CLJ <lijunli2598@gmail.com>
2013-11-08 18:20:51 +0800

    add 1 test

--- benchs/iter_all_docs/test_stub_file_open_tcp.py
+++ benchs/iter_all_docs/test_stub_file_open_tcp.py
@@ -7,7 +7,7 @@ sys.path.append('../../xapian_weibo')
 from xapian_backend import XapianSearch
 
 stub = 'master_timeline_weibo_stub'
-s = XapianSearch(stub=stub)
+s = XapianSearch(stub=stub, include_remote=True)
 results = s.iter_all_docs()
 for r in results:
     pass
