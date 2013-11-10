72c1e5ce9eac24d8b7e3ac0c4cfefb7ab34ce217
MOON_CLJ <lijunli2598@gmail.com>
2013-11-10 21:20:23 +0800

    little fix

--- benchs/search/test_stub_file_open_tcp.py
+++ benchs/search/test_stub_file_open_tcp.py
@@ -8,7 +8,7 @@ sys.path.append('../../xapian_weibo')
 from xapian_backend import XapianSearch
 
 stub = 'master_timeline_weibo_stub'
-s = XapianSearch(stub=stub)
+s = XapianSearch(stub=stub, include_remote=True)
 begin_ts1 = time.mktime(datetime.datetime(2013, 1, 1).timetuple())
 end_ts1 = time.mktime(datetime.datetime(2013, 1, 15).timetuple())
 
@@ -16,7 +16,7 @@ query_dict = {
     'timestamp': {'$gt': begin_ts1, '$lt': end_ts1},
     '$not': {'retweeted_status': '0'}
 }
-_, get_results = s.search(query=query_dict, fields=['user'])
+_, get_results = s.search(query=query_dict, fields=['user', 'terms'])
 
 for r in get_results():
     pass
